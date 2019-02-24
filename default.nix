d1955d7dfdd3c92b98e262f941f8cff03fbe8c3a
Ben Gamari <ben@smart-cactus.org>
2019-02-24 13:44:39 -0500

    Use pfalcon's micropython fork

--- default.nix
+++ default.nix
@@ -4,7 +4,17 @@ in with nixpkgs;
 rec {
   gcc-xtensa = callPackage ./esp32-toolchain {};
   esp-idf = callPackage ./esp-idf { };
-  micropython-esp32 = callPackage ./micropython { inherit gcc-xtensa esp-idf; };
+
+  micropython-esp32 = micropython-pfalcon-esp32;
+  micropython-upstream-esp32 = callPackage ./micropython { inherit gcc-xtensa esp-idf; };
+  micropython-pfalcon-esp32 = (callPackage ./micropython { inherit gcc-xtensa esp-idf; }).overrideAttrs (oldAttrs: {
+    src = fetchgit {
+      url = "https://github.com/pfalcon/micropython";
+      rev = "f31d7073931242a75dc6967e86ec522b80baea28";
+      sha256 = "1zr95rln32lszv7rbz7sbi4iy149h7g3q2n7isjfa4hblgb2c9qv";
+    };
+  });
+
   flash-micropython-esp32 = writeScriptBin "flash-micropyton-esp32" ''
     PORT=$1
     if [[ -z "$PORT" ]]; then
